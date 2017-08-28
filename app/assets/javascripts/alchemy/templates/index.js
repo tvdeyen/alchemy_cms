a6689e1e2577cb3569dea89f0cf1705c6939f1ea
Thomas von Deyen <thomas@vondeyen.com>
2017-08-28 17:57:46 +0200

    Add an element handlebars template

--- app/assets/javascripts/alchemy/templates/index.js
+++ app/assets/javascripts/alchemy/templates/index.js
@@ -1,2 +1,5 @@
+//= require alchemy/templates/element/_header
+//= require alchemy/templates/element/_toolbar
+//= require alchemy/templates/element
 //= require alchemy/templates/elements_window
 //= require alchemy/templates/spinner
