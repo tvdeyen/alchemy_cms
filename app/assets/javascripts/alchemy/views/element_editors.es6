0f79eb439d87eb8883397295a31615c55d456945
Thomas von Deyen <thomas@vondeyen.com>
2017-08-28 15:08:35 +0200

    Add a element editors Backbone collection view

---
+++ app/assets/javascripts/alchemy/views/element_editors.es6
@@ -0,0 +1,5 @@
+Alchemy.Views = Alchemy.Views || {};
+
+Alchemy.Views.ElementEditors = Backbone.View.extend({
+  el: '#element_area'
+});
--- app/assets/javascripts/alchemy/views/index.js
+++ app/assets/javascripts/alchemy/views/index.js
@@ -1,6 +1,7 @@
 //= require_self
 //= require alchemy/views/preview_window
 //= require alchemy/views/element
+//= require alchemy/views/element_editors
 //= require alchemy/views/elements_window
 
 Alchemy.Views = Alchemy.Views || {};
