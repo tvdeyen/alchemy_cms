548170c49b7485b8383ed931ad5e3b8d84309d23
Thomas von Deyen <thomas@vondeyen.com>
2017-08-29 15:30:41 +0200

    Add linkable essence tools Handlebars partial

--- app/assets/javascripts/alchemy/templates/essences/index.js
+++ app/assets/javascripts/alchemy/templates/essences/index.js
@@ -1,3 +1,4 @@
+//= require alchemy/templates/essences/_linkable_essence_tools
 //= require alchemy/templates/essences/essence_text
 //= require alchemy/templates/essences/essence_richtext
 //= require alchemy/templates/essences/essence_picture
