8ab6d579ff61af53b709a18667ca17fa1fec38c8
Thomas von Deyen <thomas@vondeyen.com>
2018-01-30 15:47:20 +0100

    Add datepicker Vue component

---
+++ app/assets/javascripts/alchemy/components/datepicker.js
@@ -0,0 +1,30 @@
+//= require alchemy/alchemy.datepicker
+
+Vue.component('alchemy-datepicker', {
+  props: {
+    current: {required: true},
+    name: {type: String, required: true},
+    id: String,
+    type: String
+  },
+
+  template: `
+    <input type="text"
+      v-model="date"
+      :name="name"
+      :id="id"
+      :data-datepicker-type="picker_type">
+  `,
+
+  data() {
+    const date = this.current ? new Date(this.current) : new Date();
+    return {
+      picker_type: this.type || 'date',
+      date: date.toLocaleDateString(Alchemy.locale)
+    }
+  },
+
+  mounted() {
+    Alchemy.Datepicker(this.$el.parentNode);
+  }
+});
