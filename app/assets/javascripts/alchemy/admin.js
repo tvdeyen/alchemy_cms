c102c736189ecfcdd13ee79a237d03ace84984ff
Thomas von Deyen <thomas@vondeyen.com>
2018-01-30 14:29:17 +0100

    Remove unused JS string extension

--- app/assets/javascripts/alchemy/admin.js
+++ app/assets/javascripts/alchemy/admin.js
@@ -33,7 +33,6 @@
 //= require alchemy/alchemy.i18n
 //= require alchemy/alchemy.image_cropper
 //= require alchemy/alchemy.image_overlay
-//= require alchemy/alchemy.string_extension
 //= require alchemy/alchemy.link_dialog
 //= require alchemy/alchemy.list_filter
 //= require alchemy/alchemy.initializer
