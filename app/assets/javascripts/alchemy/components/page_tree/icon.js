672c034e533a487f33a6093cfb24d999ec5ffc4c
Thomas von Deyen <thomas@vondeyen.com>
2018-08-31 09:05:07 +0200

    Make the icon class in page icon reactive

--- app/assets/javascripts/alchemy/components/page_tree/icon.js
+++ app/assets/javascripts/alchemy/components/page_tree/icon.js
@@ -1,9 +1,13 @@
 Vue.component('alchemy-page-icon', {
   props: { page: Object },
+  data() {
+    return {
+      iconClasses: this.page.locked ? 'icon fas fa-edit fa-fw' : 'icon far fa-file fa-lg'
+    }
+  },
   render(h) {
     const page = this.page;
-    const iconClasses = page.locked ? 'icon fas fa-edit fa-fw' : 'icon far fa-file fa-lg';
-    const icon = h('i', { class: iconClasses });
+    const icon = h('i', { class: this.iconClasses });
     if (page.locked) {
       return h('span', { attrs: { class: 'with-hint' } }, [
         icon,
