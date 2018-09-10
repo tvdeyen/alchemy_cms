2514a8d54438f12d82ca9a1d97ee74bdcdb9bf55
Thomas von Deyen <thomas@vondeyen.com>
2018-09-10 20:15:21 +0200

    Load and render child pages of toggle emits event

--- app/assets/javascripts/alchemy/components/page_tree/node.js
+++ app/assets/javascripts/alchemy/components/page_tree/node.js
@@ -9,9 +9,20 @@ Vue.component('alchemy-page-node', {
     page: { type: Object, required: true }
   },
   data() {
-    const leftPadding = (this.page.level - 1) * 32;
-    const pageStyle = { paddingLeft: `${leftPadding}px` }
-    return { pageStyle }
+    return {
+      children: this.page.children
+    }
+  },
+  methods: {
+    toggle(data) {
+      if (data.folded) {
+        this.children = []
+      } else {
+        $.getJSON('/admin/pages/tree', { id: this.page.id }, (data) => {
+          this.children = data
+        })
+      }
+    }
   },
   render(h) {
     const page = this.page;
@@ -30,13 +41,19 @@ Vue.component('alchemy-page-node', {
     ]
     const leftImages = [h('alchemy-page-icon', { props: { page } })];
     if (page.has_children && !page.root) {
-      leftImages.unshift(h('alchemy-page-toggle'))
+      leftImages.unshift(h('alchemy-page-toggle', { on: { toggle: this.toggle } }))
     }
     nodes.push(h('div', { attrs: { class: 'sitemap_left_images' } }, leftImages))
     if (page.redirects_to_external) {
       nodes.push(h('alchemy-page-external-url', { props: { page } }))
     }
     nodes.push(h('alchemy-page-link', { props: { page } }))
-    return h('li', { attrs: { class: 'sitemap_page' }, style: this.pageStyle }, nodes)
+    if (this.children.length) {
+      nodes.push(h('ul', this.children.map((child) => {
+        Object.assign(child, { children: [] })
+        return h('alchemy-page-node', { props: { page: child } })
+      })))
+    }
+    return h('li', { attrs: { class: 'sitemap_page' } }, nodes)
   }
 });
