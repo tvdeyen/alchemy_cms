7f8cb4d69791ddcd38098696eee101e67ef8c72e
Thomas von Deyen <thomas@vondeyen.com>
2018-09-11 12:02:57 +0200

    Add button and label class options to dialog_button component
    And use a render function instead of a template string to improve render performance when this button is used in large lists of parent components.

--- app/assets/javascripts/alchemy/components/dialog_button.js
+++ app/assets/javascripts/alchemy/components/dialog_button.js
@@ -10,28 +10,35 @@ Vue.component('alchemy-dialog-button', {
     title: String,
     hotkey: String,
     buttonId: String,
-    size: String
+    size: String,
+    buttonClass: String,
+    labelClass: String
   },
-
-  template: `<div class="button_with_label" :id="buttonId">
-    <a :title="title" @click.prevent="openDialog" class="icon_button" :data-alchemy-hotkey="hotkey">
-      <i :class="iconClasses"></i>
-    </a><br>
-    <label>{{label}}</label>
-  </div>`,
 
   data() {
     return {
+      buttonClasses: `button_with_label ${this.buttonClass}`,
       iconClasses: `icon fas fa-fw fa-${this.iconClass}`
     }
   },
 
   methods: {
     openDialog(e) {
+      e.preventDefault()
       new Alchemy.Dialog(this.url, {
         title: this.title,
         size: this.size
       }).open();
     }
+  },
+
+  render(h) {
+    const icon = h('i', { class: this.iconClasses });
+    const label = h('label', { attrs: { class: this.labelClass } }, [this.label])
+    const link = h('a', { attrs: {
+      class: 'icon_button', 'data-alchemy-hotkey': this.hotkey },
+      on: { click: this.openDialog }
+    }, [icon]);
+    return h('div', { attrs: { class: this.buttonClasses, id: this.buttonId } }, [link, label] )
   }
 });
