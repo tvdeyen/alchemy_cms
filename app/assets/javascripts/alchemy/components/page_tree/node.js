9ea63138c81fcdea5a4d70f8f7424c4ef225cde6
Thomas von Deyen <thomas@vondeyen.com>
2018-09-11 12:52:09 +0200

    Add sitemap tools buttons

--- app/assets/javascripts/alchemy/components/page_tree/node.js
+++ app/assets/javascripts/alchemy/components/page_tree/node.js
@@ -3,6 +3,7 @@
 //= require alchemy/components/page_tree/page_status
 //= require alchemy/components/page_tree/page_external_url
 //= require alchemy/components/page_tree/toggle
+//= require alchemy/components/page_tree/button
 
 Vue.component('alchemy-page-node', {
   props: {
@@ -26,7 +27,73 @@ Vue.component('alchemy-page-node', {
   },
   render(h) {
     const page = this.page;
+
     const nodes = [
+      h('div', { attrs: { class: 'sitemap_right_tools' } }, [
+        h('alchemy-sitemap-button', {
+          props: {
+            icon: 'info-circle',
+            url: `/admin/pages/${page.id}/info`,
+            label: 'Page info',
+            dialog: {
+              title: 'Page info',
+              size: '520x290'
+            },
+            disabled: !page.permissions.info,
+            disabledNotice: 'Your user role does not allow you to see info about this page'
+          }
+        }),
+        h('alchemy-sitemap-button', {
+          props: {
+            icon: 'cog',
+            url: `/admin/pages/${page.id}/configure`,
+            label: 'Edit page properties',
+            dialog: {
+              title: 'Edit page properties',
+              size: page.redirects_to_external ? '450x330' : '450x680'
+            },
+            disabled: !page.permissions.configure,
+            disabledNotice: 'Your user role does not allow you to edit this pages properties'
+          }
+        }),
+        h('alchemy-sitemap-button', {
+          props: {
+            icon: 'copy',
+            url: Alchemy.routes.copy_admin_page_path(page.id),
+            urlMethod: 'POST',
+            label: 'Copy page',
+            disabled: !page.permissions.copy,
+            disabledNotice: 'Your user role does not allow you to edit this pages content'
+          }
+        }),
+        h('alchemy-sitemap-button', {
+          props: {
+            icon: 'minus',
+            url: `/admin/pages/${page.id}`,
+            urlMethod: 'DELETE',
+            label: 'Delete this page',
+            confirm: {
+              message: 'Do you really want to delete this page?',
+            },
+            disabled: !page.permissions.destroy,
+            disabledNotice: 'Your user role does not allow you to delete this page'
+          }
+        }),
+        h('alchemy-sitemap-button', {
+          props: {
+            icon: 'plus',
+            url: `/admin/pages/new?parent_id=${page.id}`,
+            label: 'Create a new subpage',
+            dialog: {
+              title: 'Create a new subpage',
+              size: '340x165'
+            },
+            disabled: !page.permissions.create,
+            disabledNotice: 'Your user role does not allow you to create a new subpage'
+          }
+        })
+      ]),
+
       h('div', { attrs: { class: 'page_infos' } }, [
         h('alchemy-page-status', {
           props: { hint: page.status_titles.public, icon: 'compass', disabled: !page.public }
@@ -39,21 +106,26 @@ Vue.component('alchemy-page-node', {
         })
       ])
     ]
+
     const leftImages = [h('alchemy-page-icon', { props: { page } })];
     if (page.has_children && !page.root) {
       leftImages.unshift(h('alchemy-page-toggle', { on: { toggle: this.toggle } }))
     }
     nodes.push(h('div', { attrs: { class: 'sitemap_left_images' } }, leftImages))
+
     if (page.redirects_to_external) {
       nodes.push(h('alchemy-page-external-url', { props: { page } }))
     }
+
     nodes.push(h('alchemy-page-link', { props: { page } }))
+
     if (this.children.length) {
       nodes.push(h('ul', this.children.map((child) => {
         Object.assign(child, { children: [] })
         return h('alchemy-page-node', { props: { page: child } })
       })))
     }
+
     return h('li', { attrs: { class: 'sitemap_page' } }, nodes)
   }
 });
