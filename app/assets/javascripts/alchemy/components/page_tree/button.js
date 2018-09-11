b1ef485161b014ab1003a2a6256cbc3413aff9c0
Thomas von Deyen <thomas@vondeyen.com>
2018-09-11 12:27:23 +0200

    Add confirm option to sitemap button

--- app/assets/javascripts/alchemy/components/page_tree/button.js
+++ app/assets/javascripts/alchemy/components/page_tree/button.js
@@ -1,13 +1,16 @@
 //= require alchemy/components/dialog_button
+//= require alchemy/components/confirm_button
 
 Vue.component('alchemy-sitemap-button', {
   props: {
     url: String,
+    urlMethod: String,
     icon: String,
     label: String,
     disabled: { type: Boolean, default: false },
     disabledNotice: String,
-    dialog: Object
+    dialog: Object,
+    confirm: Object
   },
   data() {
     return {
@@ -30,6 +33,19 @@ Vue.component('alchemy-sitemap-button', {
           label: this.label,
           title: this.dialog.title,
           size: this.dialog.size,
+          buttonClass: 'sitemap_tool',
+          labelClass: 'center'
+        }
+      })
+    }
+    if (this.confirm) {
+      return h('alchemy-confirm-button', {
+        props: {
+          url: this.url,
+          urlMethod: this.urlMethod,
+          icon: this.icon,
+          label: this.label,
+          message: this.confirm.message,
           buttonClass: 'sitemap_tool',
           labelClass: 'center'
         }
