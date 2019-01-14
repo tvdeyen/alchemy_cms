20dcda0e11c58d96433e1bf6c629af3af1524c98
Thomas von Deyen <thomas@vondeyen.com>
2019-01-14 22:46:02 +0100

    Add handlebars-loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
 const { environment } = require('@rails/webpacker')
-const coffee =  require('./loaders/coffee')
+const coffee = require('./loaders/coffee')
+const handlebars = require('./loaders/handlebars')
 
 environment.loaders.prepend('coffee', coffee)
+environment.loaders.prepend('handlebars', handlebars)
 module.exports = environment
